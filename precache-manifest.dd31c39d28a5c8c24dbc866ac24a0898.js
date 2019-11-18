36321099b2fa0a5dd07ed9727332ef437972d603
Yury <goldyukol12@gmail.com>
2019-11-18 14:11:45 +0300

    Updates

---
+++ precache-manifest.dd31c39d28a5c8c24dbc866ac24a0898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23befdd9a374de417a7eb5104fce466c",
+    "url": "/DogApp/index.html"
+  },
+  {
+    "revision": "990b0361b07839c1e533",
+    "url": "/DogApp/static/css/main.05152123.chunk.css"
+  },
+  {
+    "revision": "09731b1300da5ca4f08d",
+    "url": "/DogApp/static/js/2.34d1ef0a.chunk.js"
+  },
+  {
+    "revision": "990b0361b07839c1e533",
+    "url": "/DogApp/static/js/main.090f9212.chunk.js"
+  },
+  {
+    "revision": "642f5d1fcf2072161274",
+    "url": "/DogApp/static/js/runtime-main.31ab2690.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/DogApp/static/media/dog.75fa68d4.mp3"
+  }
+]);
